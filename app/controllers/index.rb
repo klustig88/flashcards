fbc6650b2c0052b69c293f47fa697af1fef15b22
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-20 11:43:52 -0500

    initial wireframe

---
+++ app/controllers/index.rb
@@ -0,0 +1,35 @@
+get '/' do
+  # Look in app/views/index.erb
+  erb :index
+end
+
+get '/game/question/id' do
+  erb :question
+end
+
+
+get '/results' do
+  erb :results
+end
+
+get '/signup' do
+  erb :sign_up
+end
+
+get '/logout' do
+  redirect to ('/')
+end
+
+
+#______________________________post
+
+post '/create_user' do
+
+redirect to ('/')
+end
+
+post '/login' do
+  redirect to ('/game/question/id')
+end
+
+
